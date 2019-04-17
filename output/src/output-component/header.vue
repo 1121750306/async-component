acc9f207197b62e73b3ae2fe8f81e1c3737a6802
王安宇 <ouyasusakai@wanganyudeMacBook-Pro.local>
2019-04-17 16:47:01 +0800

    init

---
+++ output/src/output-component/header.vue
@@ -0,0 +1,21 @@
+<style lang="less">
+.output-header {
+  background-color: #789;
+  height: 50px;
+  line-height: 50px;
+  text-align: left;
+  text-indent: 20px;
+}
+</style>
+
+<template>
+  <header class="output-header">
+    这是header
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'outputHeader',
+};
+</script>
